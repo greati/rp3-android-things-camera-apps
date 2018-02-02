cb432284ddd2b28843095edbb43e68d5ca20da6b
Vitor Greati <greati@ufrn.edu.br>
2018-02-02 15:34:25 -0300

    Start simple camera preview tests

---
+++ simple-camera-preview/app/src/main/java/com/example/androidthings/doorbell/DoorbellActivity.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2016, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.androidthings.doorbell;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.ImageView;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Doorbell activity that capture a picture from the Raspberry Pi 3
+ * Camera on a button press and post it to Firebase and Google Cloud
+ * Vision API.
+ */
+public class DoorbellActivity extends Activity {
+    private static final String TAG = DoorbellActivity.class.getSimpleName();
+
+    //private FirebaseDatabase mDatabase;
+    private DoorbellCamera mCamera;
+
+    /*
+     * Driver for the doorbell button;
+     */
+    //private ButtonInputDriver mButtonInputDriver;
+
+    /**
+     * A {@link Handler} for running Camera tasks in the background.
+     */
+    private Handler mCameraHandler;
+
+    /**
+     * An additional thread for running Camera tasks that shouldn't block the UI.
+     */
+    private HandlerThread mCameraThread;
+
+    /**
+     * A {@link Handler} for running Cloud tasks in the background.
+     */
+    private Handler mCloudHandler;
+
+    /**
+     * An additional thread for running Cloud tasks that shouldn't block the UI.
+     */
+    //private HandlerThread mCloudThread;
+
+    /**
+     * To show the picture taken.
+     */
+    ImageView iv_picture_taken;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "Doorbell Activity created.");
+
+        // We need permission to access the camera
+        if (checkSelfPermission(Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // A problem occurred auto-granting the permission
+            Log.d(TAG, "No permission");
+            return;
+        }
+
+        setContentView(R.layout.show_image);
+
+        iv_picture_taken = (ImageView) findViewById(R.id.picture_taken);
+
+        mCameraThread = new HandlerThread("CameraBackground");
+        mCameraThread.start();
+        mCameraHandler = new Handler(mCameraThread.getLooper());
+
+        mCamera = DoorbellCamera.getInstance();
+        mCamera.initializeCamera(this, mCameraHandler, mOnImageAvailableListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mCamera != null)
+            mCamera.shutDown();
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Doorbell rang!
+            Log.d(TAG, "button pressed");
+            mCamera.takePicture();
+            return true;
+        }
+        return super.onKeyUp(keyCode, event);
+    }
+
+    /**
+     * Listener for new camera images.
+     */
+    private ImageReader.OnImageAvailableListener mOnImageAvailableListener =
+            new ImageReader.OnImageAvailableListener() {
+        @Override
+        public void onImageAvailable(ImageReader reader) {
+            Image image = reader.acquireLatestImage();
+            // get image bytes
+            ByteBuffer imageBuf = image.getPlanes()[0].getBuffer();
+            final byte[] imageBytes = new byte[imageBuf.remaining()];
+            imageBuf.get(imageBytes);
+            image.close();
+
+            onPictureTaken(imageBytes);
+        }
+    };
+
+    /**
+     * Handle image processing in Firebase and Cloud Vision.
+     */
+    private void onPictureTaken(final byte[] imageBytes) {
+        if (imageBytes != null) {
+            Log.d("Success!", "Picture taken");
+            runOnUiThread(new Runnable(){
+                @Override
+                public void run() {
+                    iv_picture_taken.setImageBitmap(BitmapFactory.decodeByteArray(imageBytes,0,imageBytes.length));
+                }
+            });
+        }
+    }
+}
